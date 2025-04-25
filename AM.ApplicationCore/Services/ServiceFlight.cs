c25eb4af1f097eb0f2ae1831a2c7ef165e8aa78c
SeifZnati <seifeddine.znati@esprit.tn>
2025-04-25 10:33:59 +0100

    Barcha Hajet

---
+++ AM.ApplicationCore/Services/ServiceFlight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+
+namespace AM.ApplicationCore.Services
+{
+    public class ServiceFlight : Service<Flight>, IServiceFlight
+    {
+        IUnitOfWork unitOfWork;
+        public ServiceFlight(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+    }
+}
